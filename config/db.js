f28260a2a52080fc44bb430666fa66fdd00248c8
omarpaul06 <ccispichitomar@gmail.com>
2024-02-14 15:29:52 -0500

    Bd

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ import  Sequelize  from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    
     define: {
         timestamps: false
     },
